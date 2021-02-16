a4b58442b9c685db4a4ec63a0a0bded8f8f6e744
Eli <72849929+lumas13@users.noreply.github.com>
2021-02-16 22:10:59 +0800

    Completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     float playerSpeed = 5f;
+    int coin = 0;
+
+    public Text scoreText;
+    public GameObject GameOver;
+    public AudioClip[] audioClip;
+
+    private AudioSource audioSource;
+    private Animator animator;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameOver.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,11 +30,6 @@ public class PlayerController : MonoBehaviour
     {
         PlayerMovement();
         Restart();
-        
-        //Destroy coin/saw (done)
-        //Background sound
-        //Audio sound play when die/collect 
-        //Press r to restart (done)
     }
 
     private void PlayerMovement()
@@ -30,11 +38,17 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector2.left * playerSpeed * Time.deltaTime); //Player movement
             transform.localScale = new Vector2(-2, 2); //Player turning left and right
+            animator.SetBool("idle", true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
             transform.localScale = new Vector2(2, 2);
+            animator.SetBool("idle", true);
+        }
+        else
+        {
+            animator.SetBool("idle", false);
         }
     }
 
@@ -51,11 +65,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Coin"))
         {
             Destroy(collision.gameObject); //Destory the collided object
+            coin++;
+            scoreText.GetComponent<Text>().text = "Score: " + coin;
+            audioSource.PlayOneShot(audioClip[0]);
+
         }
 
         if (collision.gameObject.CompareTag("Saw"))
         {
             Destroy(collision.gameObject);
+            GameOver.SetActive(true);
+            GetComponent<PlayerController>().enabled = false;
+            audioSource.PlayOneShot(audioClip[1]);
         }
     }
 }
